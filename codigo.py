99200f769af551609b22709bbee7041fb4153841
Sergio Hideki Sato <satohsergio@hotmail.com>
2025-06-13 15:21:16 -0300

    meu segundo commit

--- codigo.py
+++ codigo.py
@@ -1,16 +1,11 @@
 import pandas as pd
-import requests
+import os
 
-# URL do CSV de domínios do governo brasileiro
-url = 'https://dominios.governo.gov.br/dados-abertos/dominios.csv'
-
-# Baixa o arquivo CSV
-response = requests.get(url)
-with open('dominios_gov_br.csv', 'wb') as f:
-    f.write(response.content)
+# Caminho para a pasta Downloads do usuário atual
+caminho_downloads = os.path.expanduser('~/Downloads/dominios.csv')
 
 # Carrega o CSV
-df = pd.read_csv('dominios_gov_br.csv', sep=';', encoding='utf-8')
+df = pd.read_csv(caminho_downloads, sep=';', encoding='utf-8')
 
 # Supondo que a coluna com o domínio se chama 'nome'
 df['link'] = 'https://' + df['nome']
